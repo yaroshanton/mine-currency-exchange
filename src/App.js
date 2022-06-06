0ed3d15d83a6dd94da0a9a1fbbb3a817a22c8bdd
Yarosh Anton <on.yaroshanton@gmail.com>
2022-06-07 00:54:16 +0300

    added gh-pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import TextField from '@mui/material/TextField';
-import MenuItem from '@mui/material/MenuItem';
+import { TextField, MenuItem } from '@mui/material';
 import './styles.scss';
 
 function App() {
